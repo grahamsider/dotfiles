8662c7390399cb93305f9d4ba07dca1541dd5614
grahamsider <graham.sider@mail.utoronto.ca>
2018-05-19 00:50:21 +0000

    Updated polybar config, added launch script (called in i3 config)

---
+++ .config/polybar/plaunch.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+sleep 0.1
+
+# Launch top bar
+polybar -c=/root/.config/polybar/config top &
+
+# Done
