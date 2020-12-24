dc0bb67d2fa4647037c223d6203641bbfd4f414d
grahamsider <graham.sider@mail.utoronto.ca>
2020-12-24 14:43:05 -0500

    Update polybar conf

--- .config/polybar/plaunch.sh
+++ .config/polybar/plaunch.sh
@@ -1,18 +1,20 @@
-#!/usr/bin/env sh
+#!/bin/sh
+
+PBAR_DIR="$HOME/.config/polybar"
+PBAR_APPLET_RC_PATH="$PBAR_DIR/.pbar-applet-rc"
+APPLET_STATUS=$(grep "PBAR_APPLET" $PBAR_APPLET_RC_PATH | sed "s/^.*=//")
 
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
 sleep 0.1
 
-# Set environment variable
-sed -i '11s/.*/export PBAR_APPLET=false/' $HOME/.bash_profile
-source $HOME/.bash_profile
-
 # Launch top bar
-polybar -c=$HOME/.config/polybar/config top &
-
-# Done
+if [ "$APPLET_STATUS" = "true" ]; then
+    polybar -c=$HOME/.config/polybar/config_applet top &
+else
+    polybar -c=$HOME/.config/polybar/config top &
+fi
